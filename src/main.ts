58a62a662857e64669f362257aad7391c0c5f112
jos <jos@mailplus.nl>
2023-03-24 13:03:42 +0100

    Initial setup project with example code

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/core'
+import {paginateRest} from '@octokit/plugin-paginate-rest'
+import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods)
+    const octokit = new MyOctokit({auth: core.getInput('githubToken')})
+    const packageOwner: string = core.getInput('packageOwner')
+    const packageName: string = core.getInput('packageName')
+    const packageType: typeof octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg.prototype.package_type =
+      core.getInput('packageType')
+    const packageVersionName: string = core.getInput('packageVersionName')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`packageOwner: ${packageOwner}`)
+    core.debug(`packageName: ${packageName}`)
+    core.debug(`packageType: ${packageType}`)
+    core.debug(`packageVersionName: ${packageVersionName}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    const packages =
+      await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+        org: packageOwner,
+        package_type: packageType,
+        package_name: packageName
+      })
+    core.setOutput('packages:', packages)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
