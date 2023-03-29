a0be894e5ee6d436a9a7a7065dc17e6a2acae079
bob <bob.brinks@spotler.nl>
2023-03-29 12:33:52 +0200

    Implemented the delete package action

--- dist/index.js
+++ dist/index.js
@@ -46,18 +46,39 @@ function run() {
             const octokit = new MyOctokit({ auth: core.getInput('githubToken') });
             const packageOwner = core.getInput('packageOwner');
             const packageName = core.getInput('packageName');
+            const maxAgeDays = Number(core.getInput('maxAgeDays'));
+            const dryRun = core.getInput('packageName') === 'true';
+            const deleteVersionRegex = new RegExp(core.getInput('deleteVersionRegex')).compile();
             const packageType = core.getInput('packageType');
-            const packageVersionName = core.getInput('packageVersionName');
             core.debug(`packageOwner: ${packageOwner}`);
             core.debug(`packageName: ${packageName}`);
             core.debug(`packageType: ${packageType}`);
-            core.debug(`packageVersionName: ${packageVersionName}`);
-            const packages = yield octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+            const packages = yield octokit.paginate(octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg, {
                 org: packageOwner,
                 package_type: packageType,
                 package_name: packageName
+            }, response => {
+                return response.data
+                    .filter(v => deleteVersionRegex.test(v.name))
+                    .filter(v => {
+                    const difference = Math.abs(new Date(v.created_at).getTime() - new Date().getTime());
+                    const daysOld = Math.ceil(difference / (1000 * 3600 * 24));
+                    return daysOld > maxAgeDays;
+                })
+                    .map(v => {
+                    core.info(`Matched version ${v.id} ${v.description}`);
+                    if (!dryRun) {
+                        octokit.rest.packages.deletePackageVersionForOrg({
+                            org: packageOwner,
+                            package_type: packageType,
+                            package_name: packageName,
+                            package_version_id: v.id
+                        });
+                    }
+                    return v.name;
+                });
             });
-            core.setOutput('packages:', packages);
+            core.info(`Versions deleted ${packages.join(',')}`);
         }
         catch (error) {
             if (error instanceof Error)
