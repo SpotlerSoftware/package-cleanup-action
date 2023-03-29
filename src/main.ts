e408d0770adb669f613e35144cd4a6c48f18b3e4
bob <bob.brinks@spotler.nl>
2023-03-29 12:06:01 +0200

    Implemented the delete package action

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,49 @@
-import * as core from '@actions/core'
-import {Octokit} from '@octokit/core'
-import {paginateRest} from '@octokit/plugin-paginate-rest'
-import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/core';
+import { paginateRest } from '@octokit/plugin-paginate-rest';
+import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods';
 
 async function run(): Promise<void> {
   try {
-    const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods)
-    const octokit = new MyOctokit({auth: core.getInput('githubToken')})
+    const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods);
+    const octokit = new MyOctokit({auth: core.getInput('githubToken') })
     const packageOwner: string = core.getInput('packageOwner')
     const packageName: string = core.getInput('packageName')
+    const dryRun: boolean = core.getInput('packageName') === 'true'
+    const deleteVersionRegex: RegExp = new RegExp(
+      core.getInput('deleteVersionRegex')
+    ).compile()
     const packageType: typeof octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg.prototype.package_type =
       core.getInput('packageType')
-    const packageVersionName: string = core.getInput('packageVersionName')
 
     core.debug(`packageOwner: ${packageOwner}`)
     core.debug(`packageName: ${packageName}`)
     core.debug(`packageType: ${packageType}`)
-    core.debug(`packageVersionName: ${packageVersionName}`)
 
-    const packages =
-      await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+    const packages = await octokit.paginate(
+      octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
+      {
         org: packageOwner,
         package_type: packageType,
         package_name: packageName
-      })
-    core.setOutput('packages:', packages)
+      },
+      response => {
+        return response.data
+          .filter(v => deleteVersionRegex.test(v.name))
+          .map(v => {
+            if (!dryRun) {
+              octokit.rest.packages.deletePackageVersionForOrg({
+                org: packageOwner,
+                package_type: packageType,
+                package_name: packageName,
+                package_version_id: v.id
+              })
+            }
+            return v.name
+          })
+      }
+    )
+    core.info(`Versions deleted ${packages.join(',')}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
